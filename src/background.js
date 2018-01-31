8d269336d938c01dad77fabf1ec7cc04476d8900
austin <austin.cawley@gmail.com>
2018-01-30 23:42:03 -0500

    initial single page flow
    Signed-off-by: austin <austin.cawley@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,8 +1,11 @@
-const history = {};
-
 // sit in the background, waiting for the extension button to be clicked...
 chrome.browserAction.onClicked.addListener( function(tab) {
 
 	// ...when it's clicked, run the extension on the current tab
 	// chrome.tabs.executeScript( tab.id, {file: "bundle.js"} );
+  // Update all the history items for the offset of the tab that's been clicked
+  // (new index - current index) * window.clientWidth
+  chrome.storage.sync.get('', () => {
+
+  })
 });
--- src/extension.js
+++ src/extension.js
@@ -1,42 +1,142 @@
-const $ = require('jquery');
+require('p5/lib/addons/p5.dom');
+const p5 = require('p5');
+const chromep = require('chrome-promise');
 
-/**
- * Each history obj needs html to display, x y position, and time saved
- * They should be positioned by their place in the array
- * Oldest at the head
- * 
- */
+const historyItemClassName = '--history-item';
+const historyContainerId = '--history-container';
+const angleStep = 0.01;
+const radius = 2;
+const moveStep = 0.0001;
+const fadeStep = -0.0001;
 
 /*
  * interface HistoryItem {
- * 	html: string;
- * 	position: [number, number];
+ * 	elem: p5.Element;
  * 	timeSaved: Date;
+ * 	moveSpeed: number;
+ * 	angle: number;
  * }
  */
+const sketch = (p) => {
+  let mousePageX,
+    mousePageY;
+  let historyContainer;
+
+  /*
+  Helper Functions
+   */
+  function closestNodeToMouse() {
+    if (!mousePageX || !mousePageY) {
+      return null;
+    }
+
+    // Filter out all history-items and sort by smallest area
+    const elems = document.elementsFromPoint(mousePageX, mousePageY)
+      .filter(node => {
+        return (typeof node.className === 'undefined' || node.className.indexOf(historyItemClassName) === -1)
+          // && node.clientWidth < window.innerWidth / 2
+          && node.clientHeight < window.innerHeight / 2
+      })
+      .sort((nodeA, nodeB) => {
+        return (nodeA.clientWidth * nodeA.clientHeight) - (nodeB.clientWidth * nodeB.clientHeight);
+      });
+
+    return elems.length === 0 ? null : elems[0];
+  }
+
+  function addClosestToHistory() {
+    const node = closestNodeToMouse();
+    if (node) {
+      console.log(`Adding ${node}`);
+      const elem = new p5.Element(node.cloneNode(true)); // deep clone
+      elem.parent(historyContainer);
+      elem.addClass(historyItemClassName);
+      elem.position(mousePageX, mousePageY);
+
+      console.log(mousePageX, mousePageY);
+
+      history.push({
+        elem,
+        timeSaved: new Date(),
+        moveSpeed: 1,
+        angle: p.random(0, 360),
+        opacity: 1,
+      });
+    }
+  }
+
+  function addToHistoryLoop() {
+    console.log('History is in the making.');
+    addClosestToHistory();
+    setTimeout(addToHistoryLoop, p.random(5000, 10000));
+  }
 
 
-(function () {
-	console.log('ext')
-	console.log(chrome);
-	const history = [];
+  /* p5 lifecycle functions */
 
-	// Start mouse watcher
-	let mouseX, mouseY;
-	$(document).mousemove((event) => {
-		mouseX = event.pageX;
-		mouseY = event.pageY;
-	});
-	function closestMouseElement() {
-		return document.elementFromPoint(mouseX, mouseY);
-	}
+  p.setup = () => {
+    p.createCanvas(0, 0);
+    p.frameRate(60);
+    historyContainer = p.createDiv('')
+      .id(historyContainerId)
+      .parent(document.body);
 
+    addToHistoryLoop();
+  };
 
-	function addToHistoryLoop() {
-		const el = closestMouseElement();
-		history.push(el);
-		setTimeout(addToHistoryLoop, 1000);
-	}
-	addToHistoryLoop();
+  p.draw = () => {
+    // A hacky way to ensure no race conditions
+    // #noLoop at top
+    // #loop in callback
+    p.noLoop();
+    chrome.storage.sync.get('history', (history) => {
+      // console.log('draw' + p.frameCount);
+      for (const [index, item] of history.entries()) {
+        // console.log(index);
+        // Update opacity
+        item.elem.style('opacity', item.opacity);
 
-})();
+        // Update position
+        const { x, y } = item.elem.position();
+
+        const itemPosVec = new p5.Vector(x, y);
+        const mouseVec = new p5.Vector(mousePageX, mousePageY);
+        const distVec = new p5.Vector(mouseVec.x - x, mouseVec.y - y);
+
+        const moveVec = distVec.copy()
+          .normalize()
+          .mult(item.moveSpeed)
+          .add(p.cos(item.angle) * radius, p.sin(item.angle) * radius);
+        itemPosVec.add(moveVec);
+
+        // Update values
+        item.elem.position(itemPosVec.x, itemPosVec.y);
+        item.angle += p.random(0, angleStep);
+        // item.moveSpeed = p.constrain(moveStep + moveStep, 0.1, 100);
+        item.opacity += p.random(0, fadeStep);
+
+        // remove the forgotten
+        if (item.opacity < 0) {
+          history.splice(index, 1);
+        }
+      }
+    });
+
+  };
+
+  /**
+   * Record actual page mouse coords
+   * @param event
+   */
+  p.mouseMoved = (event) => {
+    mousePageX = event.pageX;
+    mousePageY = event.pageY;
+  };
+
+  p.mousePressed = () => {
+    addClosestToHistory();
+  }
+};
+
+// Start the sketch
+new p5(sketch);
